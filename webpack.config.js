4fc9823c4d19bbbbb31715f1f69f057ef5de807c
cam71101 <david.fisher137@gmail.com>
2020-10-05 20:26:01 +0100

    Fixed CSS error

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    // publicPath: '/BattleShips/',
+    publicPath: '/BattleShips/',
   },
   devtool: 'inline-source-map',
   devServer: {
